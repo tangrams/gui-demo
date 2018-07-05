86856620b0f8c86b8bb3bdde8f38a1b7442796a8
Peter Richardson <github@zoomy.net>
2018-07-05 09:43:14 -0700

    update

--- main.js
+++ main.js
@@ -30,7 +30,7 @@
 
     var layer = Tangram.leafletLayer({
         scene: 'scene.yaml',
-        attribution: '<a href="https://mapzen.com/tangram" target="_blank">Tangram</a> | &copy; OSM contributors | <a href="https://mapzen.com/" target="_blank">Mapzen</a>'
+        attribution: '<a href="https://github.com/tangrams/tangram" target="_blank">Tangram</a> | &copy; OSM contributors'
     });
 
     window.layer = layer;
